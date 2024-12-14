098523105404a4cd68ce2a5fb8e093c7961e908d
Yoooi <kamos21@protonmail.com>
2024-12-14 18:12:52 +0100

    Implement ToolBarContent as DependencyProperty

--- Source/MultiFunPlayer/Plugin/PluginViewBase.xaml.cs
+++ Source/MultiFunPlayer/Plugin/PluginViewBase.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using PropertyChanged;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 
@@ -7,9 +8,18 @@ namespace MultiFunPlayer.Plugin;
 /// <summary>
 /// Interaction logic for PluginViewBase.xaml
 /// </summary>
+[DoNotNotify]
 public partial class PluginViewBase : UserControl
 {
-    public object ToolBarContent { get; set; }
+    public static readonly DependencyProperty ToolBarContentProperty =
+        DependencyProperty.Register(nameof(ToolBarContent), typeof(object),
+                typeof(PluginViewBase), new FrameworkPropertyMetadata(null));
+
+    public object ToolBarContent
+    {
+        get => GetValue(ToolBarContentProperty);
+        set => SetValue(ToolBarContentProperty, value);
+    }
 
     public static readonly DependencyProperty StatusForegroundProperty =
         DependencyProperty.Register(nameof(StatusForeground), typeof(Brush),
