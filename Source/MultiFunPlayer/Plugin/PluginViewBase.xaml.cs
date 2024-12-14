d6720c5c1210c79a8d64529b7ca6c94b9924080a
Yoooi <kamos21@protonmail.com>
2024-12-14 02:01:11 +0100

    Add support for status in plugin container tab item

--- Source/MultiFunPlayer/Plugin/PluginViewBase.xaml.cs
+++ Source/MultiFunPlayer/Plugin/PluginViewBase.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace MultiFunPlayer.Plugin;
 
@@ -8,6 +10,26 @@ namespace MultiFunPlayer.Plugin;
 public partial class PluginViewBase : UserControl
 {
     public object ToolBarContent { get; set; }
+
+    public static readonly DependencyProperty StatusForegroundProperty =
+        DependencyProperty.Register(nameof(StatusForeground), typeof(Brush),
+            typeof(PluginViewBase), new FrameworkPropertyMetadata(SystemColors.ControlTextBrush));
+
+    public Brush StatusForeground
+    {
+        get => (Brush)GetValue(StatusForegroundProperty);
+        set => SetValue(StatusForegroundProperty, value);
+    }
+
+    public static readonly DependencyProperty StatusContentProperty =
+        DependencyProperty.Register(nameof(StatusContent), typeof(object),
+            typeof(PluginViewBase), new FrameworkPropertyMetadata(null));
+
+    public object StatusContent
+    {
+        get => GetValue(StatusContentProperty);
+        set => SetValue(StatusContentProperty, value);
+    }
 
     public PluginViewBase()
     {
